ce373968c287f377c7d22b467575535b11afc987
matveev96 <sasha.matveev1996@icloud.com>
2024-11-15 08:28:42 +0300

    HW2 is Done

--- src/HW/2/HW2.tsx
+++ src/HW/2/HW2.tsx
@@ -10,11 +10,11 @@ export type UserType = {
   id: number;
   name: string;
   age: number;
-  address: any; // пропиши типизацию
+  address: AddressType; // пропиши типизацию
 };
 
 export type UsersObjectType = {
-  myFriends: any; // пропиши типизацию
+  myFriends: UserType[]; // пропиши типизацию
 };
 
 export const HW2 = () => {
@@ -45,10 +45,12 @@ export const HW2 = () => {
   }
 
   let [currentUsers, setCurrentUsers] = useState<UsersObjectType>(users);
+  console.log(currentUsers)
 
-  const filterUsers = () => {
-    const filteredUsers = 'НУЖНО ПРОФИЛЬТРОВАТЬ ДРУЗЕЙ. ОСТАВЛЯЕМ ТОЛЬКО ТЕХ, КОТОРЫЕ ЖИВУТ В ГОРОДЕ LOS ANGELES';
+  const filterUsers = (cityArg: string) => {
+    const filteredUsers = users.myFriends.filter(user => user.address.city === cityArg);
     setCurrentUsers({ myFriends: filteredUsers });
+
   };
 
   return (
